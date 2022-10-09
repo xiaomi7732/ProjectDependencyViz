29660ee53ec9bb91807c49955c7abe908eaa9b9c
Saar Shen <xiaomi7732@hotmail.com>
2022-10-08 22:26:04 -0700

    Add a script for releasing

---
+++ release.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+dotnet build -c Release --no-incremental
+dotnet publish -c Release -o ./out --no-build
+cp ./out/wwwroot/index.html ./out/wwwroot/404.html
+cp ./out/wwwroot/* ../docs -r
+touch ..docs/.nojekyll
